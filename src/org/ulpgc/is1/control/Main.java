b38a6d8d07180862b10f316c2244fd4f362593f3
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-01-14 18:54:58 +0000

    Commit Main

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -10,41 +10,34 @@ import java.util.List;
 public class Main {
 
     public static void init(RepairManager repairManager) throws Exception {
+        RepairManager.addVehicle("Jeep", "Renegade", new Plate("4450LMD"), new Customer("Javier", new Phone("664142307")));
+        RepairManager.addVehicle("Porche", "911", new Plate("6977MMM"), new Customer("Manuel", new Phone("674332819")));
 
-        //Creación de mecánicos
-        repairManager.addMechanic("Francisco Javier", "Monleón", "Peña");
-        repairManager.addMechanic("Daniel", "Parrilla", "Acosta");
+        RepairManager.addMechanic("Francisco Javier", "Monleón", "Peña");
+        RepairManager.addMechanic("Daniel", "Parrilla", "Acosta");
 
-        //Creación de vehículos
-        repairManager.addVehicle("Jeep", "Renegade", new Plate("4450LMD"), new Customer("Javier", new Phone("664142307")));
-        repairManager.addVehicle("Porche", "911", new Plate("6977MMM"), new Customer("Manuel", new Phone("674332819")));
-
-        //Creación de piezas
-        repairManager.repairs("Neumático Radial", 4);
-        repairManager.repairs("Motor Diesel", 15);
+        RepairManager.repairs("Neumático Radial", 4);
+        RepairManager.repairs("Motor Diesel", 15);
+        RepairManager.repairs("Parabrisas", 18);
+        RepairManager.repairs("Puerta Maletero", 1);
     }
 
     public static void main(String[] args) throws Exception {
 
+        RepairManager.repairs(new Date(), "Reparación de Jeep Renegade por Daniel Parrilla Acosta", 100, repairManager.getVehicle().get(1), BreakdownTypes.MECHANICAL, repairManager.getMechanic().get(0));
+        Vehicle.getRepairList().get(0).addPayment(1, RepairManager.getSparePart().get(0));
+        Vehicle.getRepairList().get(0).addPayment(2, RepairManager.getSparePart().get(1));
+
         RepairManager repairManager = new RepairManager();
         init(repairManager);
-
-        System.out.println("----------------------");
         System.out.println(repairManager.getMechanic().get(0).toString());
+        System.out.println(RepairManager.getVehicle().get(1).toString());
+
+        Vehicle.getRepairList().get(0).addPayment(new Payment(new Date(), RepairManager.getRepairs().get(0).price()));
+
+        RepairManager.removeVehicle(RepairManager.getVehicle().get(0));
+        System.out.println("Nºvehiculos: " + RepairManager.getVehicle().size());
         System.out.println("----------------------");
-        System.out.println(repairManager.getVehicle().get(1).toString());
-        System.out.println("----------------------");
-
-        repairManager.repairs(new Date(), "Reparación de Jeep Renegade por Daniel Parrilla Acosta", 100, repairManager.getVehicle().get(1), BreakdownTypes.MECHANICAL, repairManager.getMechanic().get(0));
-        Vehicle.getRepairList().get(0).addPayment(1, repairManager.getSparePart().get(0));
-        Vehicle.getRepairList().get(0).addPayment(2, repairManager.getSparePart().get(1));
-
-        Vehicle.getRepairList().get(0).addPayment(new Payment(new Date(), repairManager.getRepairs().get(0).price()));
-
-        repairManager.removeVehicle(repairManager.getVehicle().get(0));
-        System.out.println("Nºvehiculos: " + repairManager.getVehicle().size());
-        System.out.println("----------------------");
-
         System.out.println(Vehicle.getRepairList().get(0).toString());
         System.out.println("----------------------");
 
