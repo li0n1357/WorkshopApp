17beab60c33df8ec005c09d5f025a8e17e5b193f
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-01-14 18:22:24 +0000

    Commit Vehicle corregido

--- src/org/ulpgc/is1/model/Vehicle.java
+++ src/org/ulpgc/is1/model/Vehicle.java
@@ -10,7 +10,7 @@ public class Vehicle {
     private String model;
     private Plate plate;
     private Customer owner;
-    private List<Repair> repairList;
+    private static List<Repair> repairList;
 
     public Vehicle(String make, String model, Plate plate, Customer owner) {
         this.make = make;
