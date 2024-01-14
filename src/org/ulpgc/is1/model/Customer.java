691d5ac2b11483f5f5126a4964d88c3eb65c186a
Javier Monleón Peña <piticat.tnf@gmail.com>
2024-01-14 13:12:35 +0000

    Commit customer corregido

--- src/org/ulpgc/is1/model/Customer.java
+++ src/org/ulpgc/is1/model/Customer.java
@@ -1,12 +1,16 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Customer {
+
     private String name;
     private Phone phone;
-
+    private List<Vehicle> vehicleList;
     public Customer(String name, Phone phone) {
         this.name = name;
         this.phone = phone;
+        this.vehicleList = new ArrayList<>();
     }
 
     public String getName() {
@@ -24,10 +28,13 @@ public class Customer {
     public void setPhone(Phone phone) {
         this.phone = phone;
     }
-
-    public static Customer createCustomer(String name, String phoneNumber) {
-        Phone phone = new Phone(phoneNumber);
-        return new Customer(name, phone);
+    public List<Vehicle> getVehicleList() {
+        return vehicleList;
     }
+    public void addVehicle(Vehicle vehicle){
+        if(!(vehicleList.contains(vehicle))) vehicleList.add(vehicle);
+    }
+    public void removeVehicle(Vehicle vehicle){
+        vehicleList.remove(vehicle);
+                }
 }
-
